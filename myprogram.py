23332ff74d6438fda53d7821588e83256d635ec2
vladimirmaric <vladimirmaric@icloud.com>
2019-10-14 14:52:27 +0200

    ppf-ex06

--- myprogram.py
+++ myprogram.py
@@ -1,6 +1,11 @@
-x = 10
-print(id(x))
- 
-x = 10
+search = 15
+for i in range(10):
+    print("Current number: " , i)
+    if(i==search):
+        print("Number found!")
+        break
+else:  
+    print("Number not found")
 
-print(id(x))
+
+
